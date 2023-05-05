7e1989a716db13710e9c3aa2bb3df9a3e734d0d6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-05 15:45:54 +0300

    Wrote a simple test to determine if the url is correct

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+	render(<App />)
+	const linkElement = screen.getByText(/learn react/i)
+	expect(linkElement).toBeInTheDocument()
+})
+
+test('URL is correct', () => {
+	render(<App />)
+	const linkElement = screen.getByText(/learn react/i)
+	expect(linkElement.getAttribute('href')).toContain('ultimateqa.com')
+})
